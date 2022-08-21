6696f2810a32dae60a1e9f78f97642b824127f62
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-08-21 21:26:03 +0200

    Add scanner code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use infrared_rs::Scanner;
+
+const PIN_NUMBER: u8 = 26;
+
 fn main() {
-    println!("Hello, world!");
+    let mut scanner = Scanner::try_new(PIN_NUMBER).unwrap();
+
+    loop {
+        println!("{}", scanner.scan_blocking().unwrap())
+    }
 }
