fab1223be665a340d2128557f82778b4042ccc08
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-08-24 15:01:52 +0200

    Add install.sh, Changelog

---
+++ README.md
@@ -0,0 +1,2 @@
+# Infrared Node - Rust Rewrite
+This is a new and improved version of [infrared-node](https://github.com/smarthome-go/infrared-node)
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+grep -q BCM /proc/cpuinfo || { echo -e "\x1b[1;31mPlease run the installer on a Raspberry pi\x1b[1;0m"; exit 1; }
+echo -e "\x1b[1;34minstallation is running\x1b[1;0m"
+
+sudo mkdir -p /etc/ifrs/
+sudo chown -R pi /etc/ifrs/ || exit 1
+mv ./infrared-node-rs /usr/bin/ifrs || exit 1
+sudo cp ./ifrs.service /lib/systemd/system/ifrs.service || exit 1
+
+# Reload Systemd
+sudo systemctl daemon-reload || exit 1
+sudo systemctl enable ifrs --now || exit 1
+
+echo -e "\x1b[1;32minstallation completed\x1b[1;0m"
